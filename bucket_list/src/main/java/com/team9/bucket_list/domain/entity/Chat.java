f9471100f88b3f6d618d86f0d85fe5c6813105a5
박은빈 <joypeb@naver.com>
2023-01-19 10:57:33 +0900

    🚀[feat] 채팅 엔티티 어노테이션 추가

--- bucket_list/src/main/java/com/team9/bucket_list/domain/entity/Chat.java
+++ bucket_list/src/main/java/com/team9/bucket_list/domain/entity/Chat.java
@@ -2,8 +2,14 @@ package com.team9.bucket_list.domain.entity;
 
 import com.team9.bucket_list.domain.enumerate.ChatType;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Chat {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "chat_id")
