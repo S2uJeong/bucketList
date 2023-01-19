90d469208fe5a7d7eb9206ece4a9e150fdbc89de
박은빈 <joypeb@naver.com>
2023-01-19 17:36:43 +0900

    🚀[feat] dto수정, 페이징 처리 추가

--- bucket_list/src/main/java/com/team9/bucket_list/domain/dto/chat/ChatInviteRequest.java
+++ bucket_list/src/main/java/com/team9/bucket_list/domain/dto/chat/ChatInviteRequest.java
@@ -1,4 +1,14 @@
 package com.team9.bucket_list.domain.dto.chat;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatInviteRequest {
+    private List<Long> memberId;
 }
