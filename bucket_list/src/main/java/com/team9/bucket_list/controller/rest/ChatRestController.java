b5d472e49b4d90cd57eb5fcd8398ccf7044d351a
박은빈 <joypeb@naver.com>
2023-01-19 10:59:11 +0900

    🚀[feat] 채팅방 페이징 처리

--- bucket_list/src/main/java/com/team9/bucket_list/controller/rest/ChatRestController.java
+++ bucket_list/src/main/java/com/team9/bucket_list/controller/rest/ChatRestController.java
@@ -4,6 +4,10 @@ import com.team9.bucket_list.domain.dto.chat.ChatRoomRequest;
 import com.team9.bucket_list.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +24,10 @@ public class ChatRestController {
     //유저 채팅목록
     //auth 나중에 추가
     @GetMapping("/")
-    public String getChatList(/*auth*/ ) {
+    public String getChatList(/*auth*/ @PageableDefault(size = 20) /*제일 마지막에 채팅이 올라온 시간이 기준 @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC)*/ Pageable pageable) {
         Long userId = 1L;
         //userId에 해당하는 채팅 리스트를 가져온다
+        chatService.getChatList(userId,pageable);
         return "";
     }
 
--- bucket_list/src/main/java/com/team9/bucket_list/service/ChatService.java
+++ bucket_list/src/main/java/com/team9/bucket_list/service/ChatService.java
@@ -1,9 +1,19 @@
 package com.team9.bucket_list.service;
 
+import com.team9.bucket_list.domain.dto.chat.ChatRoomResponse;
+import com.team9.bucket_list.domain.entity.Chat;
+import com.team9.bucket_list.domain.entity.ChatParticipant;
+import com.team9.bucket_list.domain.entity.ChatRoom;
+import com.team9.bucket_list.repository.ChatParticipantRepository;
 import com.team9.bucket_list.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +21,12 @@ import org.springframework.stereotype.Service;
 public class ChatService {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final ChatParticipantRepository chatParticipantRepository;
+
+    public Page<ChatRoomResponse> getChatList(Long memberId, Pageable pageable) {
+        Set<ChatParticipant> chatParticipantList = chatParticipantRepository.findAllByMember_Id(memberId);
+        Page<ChatRoom> chatRooms = chatRoomRepository.findAllByChatParticipantsIn(chatParticipantList,pageable);
+        return null;
+    }
 
 }
