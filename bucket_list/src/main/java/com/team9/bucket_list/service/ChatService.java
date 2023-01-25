003a74e0630d4f41d630c30677179f3c85e22e13
박은빈 <joypeb@naver.com>
2023-01-25 12:04:02 +0900

    🚀[feat] 메시지 불러오기, 메시지 db저장 비동기로 작성

--- bucket_list/src/main/java/com/team9/bucket_list/service/ChatService.java
+++ bucket_list/src/main/java/com/team9/bucket_list/service/ChatService.java
@@ -1,15 +1,20 @@
 package com.team9.bucket_list.service;
 
 import com.team9.bucket_list.domain.dto.chat.ChatInviteRequest;
+import com.team9.bucket_list.domain.dto.chat.ChatRequest;
 import com.team9.bucket_list.domain.dto.chat.ChatRoomRequest;
 import com.team9.bucket_list.domain.dto.chat.ChatRoomResponse;
 import com.team9.bucket_list.domain.entity.*;
 import com.team9.bucket_list.repository.ChatParticipantRepository;
+import com.team9.bucket_list.repository.ChatRepository;
 import com.team9.bucket_list.repository.ChatRoomRepository;
+import com.team9.bucket_list.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -20,10 +25,13 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@EnableAsync
 public class ChatService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final ChatParticipantRepository chatParticipantRepository;
+    private final MemberRepository memberRepository;
+    private final ChatRepository chatRepository;
 
     public Page<ChatRoomResponse> getChatList(Long memberId, Pageable pageable) {
         List<ChatParticipant> chatParticipantList = chatParticipantRepository.findAllByMember_Id(memberId);
@@ -57,5 +65,18 @@ public class ChatService {
         chatParticipantRepository.saveAll(chatParticipantList);
 
         //성공 리턴
+    }
+
+    public Page<ChatParticipant> messages(Long roomId, Pageable pageable) {
+        return chatParticipantRepository.findAllByChatRoom_Id(roomId,pageable);
+    }
+
+    @Async
+    public void updateMessage(ChatRequest chatRequest) {
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRequest.getRoomId()).orElseThrow(() -> new RuntimeException());
+        Member member = memberRepository.findById(chatRequest.getUserId()).orElseThrow(() -> new RuntimeException());
+
+        chatRepository.save(Chat.save(chatRequest,chatRoom,member));
+        chatRoomRepository.save(ChatRoom.messageTimeUpdate(chatRoom));
     }
 }
