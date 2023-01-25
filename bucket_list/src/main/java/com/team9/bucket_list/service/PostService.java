897e9531eaaf323d84d5dfacbfaf09d7c08a8467
Bae Ji Won <qowhxk@naver.com>
2023-01-25 11:35:49 +0900

    🚀[feat] (1) Post작성에 필요한 데이터만 사용(나머지 주석처리), (2) 위도 경도 구하는 메서드 추가

--- bucket_list/src/main/java/com/team9/bucket_list/service/PostService.java
+++ bucket_list/src/main/java/com/team9/bucket_list/service/PostService.java
@@ -5,10 +5,10 @@ import com.team9.bucket_list.domain.entity.Member;
 import com.team9.bucket_list.domain.entity.Post;
 import com.team9.bucket_list.execption.ApplicationException;
 import com.team9.bucket_list.execption.ErrorCode;
-import com.team9.bucket_list.repository.MemberRepository;
 import com.team9.bucket_list.repository.PostRepository;
 import com.team9.bucket_list.utils.map.dto.GoogleMapResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,26 +25,26 @@ import java.util.Map;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
 
     private final PostRepository postRepository;
-    private final MemberRepository memberRepository;
     // map
     @Value("${google.map.key}")
     private Object API_KEY;// 실제 서버에서 구동할때는 무조건 환경변수에 숨겨야함 절대 노출되면 안됨!!!
 
-    // ========= 유효성검사 ===========
-    // 1. findByMemberId : memberId로 member 찾아 로그인 되어있는지 확인 ->  error : 권한이 없습니다.
-    public Member checkMember(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new ApplicationException(ErrorCode.USERNAME_NOT_FOUNDED));
-    }
-    // 2. checkPostMember : post를 작성한 mem과 현재 로그인 된 mem 비교 -> INVALID_PERMISSION
-    public void checkPostMember(Long memberId, Long postMemberId) {
-        Member loginMember = checkMember(memberId);
-        Member postMember = checkMember(postMemberId);
-        if(!loginMember.equals(postMember)) throw new ApplicationException(ErrorCode.INVALID_PERMISSION);
-    }
+//    // ========= 유효성검사 ===========
+//    // 1. findByMemberId : memberId로 member 찾아 로그인 되어있는지 확인 ->  error : 권한이 없습니다.
+//    public Member checkMember(Long memberId) {
+//        return memberRepository.findById(memberId)
+//                .orElseThrow(() -> new ApplicationException(ErrorCode.USERNAME_NOT_FOUNDED));
+//    }
+//    // 2. checkPostMember : post를 작성한 mem과 현재 로그인 된 mem 비교 -> INVALID_PERMISSION
+//    public void checkPostMember(Long memberId, Long postMemberId) {
+//        Member loginMember = checkMember(memberId);
+//        Member postMember = checkMember(postMemberId);
+//        if(!loginMember.equals(postMember)) throw new ApplicationException(ErrorCode.INVALID_PERMISSION);
+//    }
     // 3. findByPostId : postId에 따른 post가 DB에 잘 있는지 확인 -> error : 없는 게시물입니다. POST_NOT_FOUND
     public Post checkPost(Long postId) {
         return postRepository.findById(postId)
@@ -53,11 +53,11 @@ public class PostService {
 
     // 작성
     @Transactional
-    public PostCreateResponse create(PostCreateRequest request, Long memberId) {
+    public PostCreateResponse create(PostCreateRequest request, String username) {
         // 로그인 되어있는지 확인하고 아니면 에러던짐
-        Member member = checkMember(memberId);
+//        Member member = checkMember(username);
         // requestDTO 안의 toEntity 메서드를 이용해 post Entity 객체를 생성한다.
-        Post post = request.toEntity(member);
+        Post post = request.toEntity();
         // request를 통해 만들어진 post를 DB에 저장한다.
         Post savedPost = postRepository.save(post);
         return PostCreateResponse.of(savedPost);
@@ -94,11 +94,11 @@ public class PostService {
     @Transactional
     public PostUpdateResponse update(PostUpdateRequest request, Long memberId, Long postId) {
         // 로그인 되어있는지 확인하고 아니면 에러던짐
-        Member member = checkMember(memberId);
+//        Member member = checkMember(memberId);
         // postid에 해당하는 post가 DB에 없으면 에러던짐
         Post post = checkPost(postId);
         // post를 쓴 멤버와 로그인 되어 있는 member가 같은 멤버가 아니면 에러던짐
-        checkPostMember(memberId, post.getId());
+//        checkPostMember(memberId, post.getId());
         /*post.setTitle(request.getTitle());
         request로 들어온 내용으로 post 안의 속성들을 set해줌으로써 DB에 저장시키고, Dto로 반환하고 시픙데,, 어떻게 하지*/
         return null;
@@ -108,12 +108,11 @@ public class PostService {
     @Transactional
     public PostDeleteResponse delete(Long postId, Long memberId) {
         // 로그인 되어있는지 확인하고 아니면 에러던짐
-        Member member = checkMember(memberId);
+//        Member member = checkMember(memberId);
         // postid에 해당하는 post가 DB에 없으면 에러던짐
         Post post = checkPost(postId);
         // post를 쓴 멤버와 로그인 되어 있는 member가 같은 멤버가 아니면 에러던짐
-        checkPostMember(memberId, post.getId());
-
+//        checkPostMember(memberId, post.getId());
         postRepository.deleteById(post.getId());
         return PostDeleteResponse.of(post);
 
@@ -145,4 +144,3 @@ public class PostService {
     }
 
 }
-
