bcf54747481f759b4bc4e6e898ef81aff685a036
Bae Ji Won <qowhxk@naver.com>
2023-01-25 11:27:32 +0900

    🚀[feat] (1) Front Post Data 받아오기(Ajax 통신 추가), (2) 세부게시물 출력

--- bucket_list/src/main/java/com/team9/bucket_list/controller/rest/PostController.java
+++ bucket_list/src/main/java/com/team9/bucket_list/controller/rest/PostController.java
@@ -1,9 +1,11 @@
 package com.team9.bucket_list.controller.rest;
+
 import com.team9.bucket_list.domain.Response;
 import com.team9.bucket_list.domain.dto.post.*;
 import com.team9.bucket_list.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -11,8 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.io.UnsupportedEncodingException;
 
-import java.util.Map;
 
 @Slf4j
 @Controller
@@ -20,35 +22,30 @@ import java.util.Map;
 @RequestMapping("/post")
 public class PostController {
 
+    @Value("${google.map.key}")
+    private Object API_KEY;// 실제 서버에서 구동할때는 무조건 환경변수에 숨겨야함 절대 노출되면 안됨!!!
+
+
     private final PostService postService;
 
     // 게시글 작성 폼 페이지 이동
     @GetMapping("/createform")
-    public String location(){
-        return "PostCreate";
+    public String movePostForm(){
+        return "Post/PostCreate";
     }
 
-    //AJAX 데이터 받아오기
-    @PostMapping(value="/save", produces = "application/json")
-    public String getPostData(@RequestBody Map<String,Object> objectMap
-    ) {
-        String address = (String) objectMap.get("address");// 프론트에서 저장할 때 adress key 안에 주소값을 넣어둠
-        PostCreateRequest postCreateRequest = new PostCreateRequest().test(address);
-        postService.create(postCreateRequest, 1L);
-        // String title = (String) objectMap.get("title");  // 이렇게 하면 가독성 나쁘니까 dto 단에서 스트림으로 만들어준다 key:val -> postCreateRequest에서 제작예정
-        // Post에 위치 저장시키는 Service 기능 추가
-        log.info("ajax return");
-        return "forward:/post/map";
+
+    // 게시글 폼에서 데이터 받아오기(Ajax 사용하여 받아옴)
+    @PostMapping(value = "/detailpost" ,produces = "application/json")
+    @ResponseBody
+    public Long getData(@RequestBody PostCreateRequest request) throws UnsupportedEncodingException {
+        String userName = "test";
+
+        PostCreateResponse response = postService.create(request,userName);       // DB에 데이터 저장
+        return response.getPostId();
     }
 
-    //== 작성 ==//
-    @PostMapping
-    // 여기서 Member는, 로그인 된 member를 뜻한다.
-    public Response<PostCreateResponse> createPost(@RequestBody PostCreateRequest request, Long memberId) {
-        PostCreateResponse response = postService.create(request,memberId);
-        log.info("Post 작성 성공 postId: {}", response.getPostId());
-        return Response.success(response);
-    }
+
 
     //== 전체조회 ==//
     @GetMapping("list")
@@ -60,29 +57,39 @@ public class PostController {
     }
 
     //== 세부조회 ==//
-    @GetMapping("/{postId}")
-    public Response<PostReadResponse> readPost(@PathVariable("postId") long postId) {
-        PostReadResponse postReadResponse = postService.read(postId);
-        log.info("Post 보기 성공");
-        return Response.success(postReadResponse);
-    }
 
-    // 게시물 데이터 출력(세부 게시물 데이터 출력)
-    @PostMapping("/map")
-    public String viewData(Model model) {
-        System.out.println("map 페이지");
-        PostReadResponse postReadResponse = postService.read(1L); // postId 임의 숫자 넣음
+    @GetMapping("{postId}")
+    public String showMap2(@PathVariable(value = "postId") Long postId, Model model){
+        PostReadResponse postReadResponse = postService.read(postId);
+
+        String title = postReadResponse.getTitle();
+        String untile = postReadResponse.getUntilRecruit();
+        String eventStart = postReadResponse.getEventStart();
+        String eventEnd = postReadResponse.getEventEnd();
+        int cost = postReadResponse.getCost();
+        int entrantNum = postReadResponse.getEntrantNum();
+        String category = postReadResponse.getCategory();
+        String location = postReadResponse.getLocation();
+        String content = postReadResponse.getContent();
         double lat = postReadResponse.getLat();
         double lng = postReadResponse.getLng();
-        log.info("lat : []", lat);
-        log.info("lng : []", lng);
-        String location = postReadResponse.getLocation();
-        log.info("ldcation : []" , location);
+
+
+        model.addAttribute("title",title);
+        model.addAttribute("untilRecruit",untile);
+        model.addAttribute("eventStart",eventStart);
+        model.addAttribute("eventEnd",eventEnd);
+        model.addAttribute("cost",cost);
+        model.addAttribute("entrantNum",entrantNum);
+        model.addAttribute("category",category);
+        model.addAttribute("content",content);
         model.addAttribute("lat",lat);
         model.addAttribute("lng",lng);
         model.addAttribute("location",location);
-        return "map";  // 이 부분 GetMapping으로 바꾸기 위해 어떻게 해야할 지 고민해봐야 함
+        model.addAttribute("apikey",API_KEY);
+        return "Post/PostDetail";
     }
+
 
     //== 수정 ==//  ---> 진행중 fetchMapping 사용 고려
     @PutMapping("/{postId}/{memberId}")
@@ -98,11 +105,5 @@ public class PostController {
         log.info("Post 삭제 성공");
         return Response.success(postDeleteResponse);
     }
-
-
-
-
-
-
-
 }
+
