2ad72a0b4ef68d9b6921cb36041cab19488da419
Bae Ji Won <qowhxk@naver.com>
2023-01-25 11:36:51 +0900

    🚀[feat] (1) entrantNum String -> int 변경, (2) category enum -> String 변경

--- bucket_list/src/main/java/com/team9/bucket_list/domain/dto/post/PostUpdateRequest.java
+++ bucket_list/src/main/java/com/team9/bucket_list/domain/dto/post/PostUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.team9.bucket_list.domain.dto.post;
 
-import com.team9.bucket_list.domain.entity.Member;
 import com.team9.bucket_list.domain.entity.Post;
 import com.team9.bucket_list.domain.enumerate.PostCategory;
 import com.team9.bucket_list.domain.enumerate.PostStatus;
@@ -20,11 +19,11 @@ public class PostUpdateRequest {
     private int cost; //비용
     private String location; //장소
     private String untilRecruit; //모집종료날짜
-    private String entrantNum; //모집인원제한
+    private int entrantNum; //모집인원제한
     private String eventStart; //버킷 시작일
     private String eventEnd; //버킷 종료일
     private PostStatus status; // 모집중 or 모집완료
-    private PostCategory category; //카테고리
+    private String category; //카테고리
 
     // ========= toEntity ===============
     public Post toEntity() {
