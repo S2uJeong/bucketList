6963f45c536859707d98a44c0d5ed0feb66621ff
박은빈 <joypeb@naver.com>
2023-01-30 13:51:48 +0900

    🚀[feat] 알람 보내기 수정

--- bucket_list/src/main/java/com/team9/bucket_list/service/AlarmService.java
+++ bucket_list/src/main/java/com/team9/bucket_list/service/AlarmService.java
@@ -3,10 +3,12 @@ package com.team9.bucket_list.service;
 import com.team9.bucket_list.domain.dto.alarm.AlarmListResponse;
 import com.team9.bucket_list.domain.entity.Alarm;
 import com.team9.bucket_list.domain.entity.Member;
+import com.team9.bucket_list.domain.entity.Post;
 import com.team9.bucket_list.execption.ApplicationException;
 import com.team9.bucket_list.execption.ErrorCode;
 import com.team9.bucket_list.repository.AlarmRepository;
 import com.team9.bucket_list.repository.MemberRepository;
+import com.team9.bucket_list.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -23,15 +25,19 @@ public class AlarmService {
 
     private final AlarmRepository alarmRepository;
     private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
 
     public Page<AlarmListResponse> alarmList(Pageable pageable, Long memberId) {
         return AlarmListResponse.toList(alarmRepository.findAllByMember_IdAndReadStatusContains(memberId,pageable, (byte)0));
     }
 
     @Async
-    public void sendAlarm(Long senderId, Long memberId, Long postId, byte category) {
+    public void sendAlarm(Long senderId, Long postId, byte category) {
         Member sender = memberRepository.findById(senderId).orElseThrow( () -> new ApplicationException(ErrorCode.USERNAME_NOT_FOUNDED));
-        Member member = memberRepository.findById(memberId).orElseThrow( () -> new ApplicationException(ErrorCode.USERNAME_NOT_FOUNDED));
+
+        Post post = postRepository.findById(postId).orElseThrow( () -> new ApplicationException(ErrorCode.POST_NOT_FOUND));
+
+        Member member = memberRepository.findById(post.getMember().getId()).orElseThrow( () -> new ApplicationException(ErrorCode.USERNAME_NOT_FOUNDED));
 
         alarmRepository.save(Alarm.save(category,member,postId, sender.getUserName()));
     }
