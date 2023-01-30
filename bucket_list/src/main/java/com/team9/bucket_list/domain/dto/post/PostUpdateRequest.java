96c4f60210ec1f55c238525c74fc62cad9263d0b
Bae Ji Won <qowhxk@naver.com>
2023-01-30 14:21:15 +0900

    🚀[feat] Post Update Dto 추가(사용 x)

--- bucket_list/src/main/java/com/team9/bucket_list/domain/dto/post/PostUpdateRequest.java
+++ bucket_list/src/main/java/com/team9/bucket_list/domain/dto/post/PostUpdateRequest.java
@@ -14,16 +14,20 @@ import lombok.NoArgsConstructor;
 @Builder
 public class PostUpdateRequest {
 
-    private String title; //제목
-    private String content; //내용
-    private int cost; //비용
-    private String location; //장소
-    private String untilRecruit; //모집종료날짜
-    private int entrantNum; //모집인원제한
-    private String eventStart; //버킷 시작일
-    private String eventEnd; //버킷 종료일
     private PostStatus status; // 모집중 or 모집완료
-    private String category; //카테고리
+    private String title; //제목 ㅇ
+    private String untilRecruit; //모집종료날짜 ㅇ
+    private String eventStart; //버킷 시작일 ㅇ
+    private String eventEnd; //버킷 종료일 ㅇ
+    private int cost; //비용
+    private int entrantNum; //모집인원제한 ㅇ
+    private String category; //카테고리  ㅇ    // 이부분은 enum으로 안해도 됨, 프론트에서 드랍다운 메뉴로 선택하게끔하고 해당 값을 보내줄수 있음
+
+    private String location; //장소 ㅇ
+
+    private String content; //내용 ㅇ
+
+
 
     // ========= toEntity ===============
     public Post toEntity() {
@@ -34,6 +38,7 @@ public class PostUpdateRequest {
                 .location(location)
                 .untilRecruit(untilRecruit)
                 .entrantNum(entrantNum)
+                .eventStart(eventStart)
                 .eventEnd(eventEnd)
                 .status(status)
                 .category(category)
