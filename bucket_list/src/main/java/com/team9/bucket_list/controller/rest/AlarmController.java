5f1f0b41233a30905f629672151106d6c0a54d9e
박은빈 <joypeb@naver.com>
2023-01-30 11:36:09 +0900

    🚀[feat] 알람 카운트 컨트롤러

--- bucket_list/src/main/java/com/team9/bucket_list/controller/rest/AlarmController.java
+++ bucket_list/src/main/java/com/team9/bucket_list/controller/rest/AlarmController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +19,22 @@ public class AlarmController {
 
     private final AlarmService alarmService;
 
+    //알람 리스트 받아오기
     @GetMapping
     public Response alarmList(@PageableDefault(size = 10) /* @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC)*/ Pageable pageable) {
         //로그인된 아이디의 알람 리스트
         Long memberId = 1L;
 
         return Response.success(alarmService.alarmList(pageable, memberId));
+    }
+
+    //알람 카운트 받아오기
+    @GetMapping
+    public Response alarmCount() {
+        //로그인된 아이디의 알람 리스트
+        Long memberId = 1L;
+
+        return Response.success(alarmService.alarmCount(memberId));
     }
 
 }
