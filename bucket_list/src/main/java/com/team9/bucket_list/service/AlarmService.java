459cb7ef2a881eac3be26deb7c8c85237ba00ebd
박은빈 <joypeb@naver.com>
2023-01-30 16:43:21 +0900

    🚀[feat] 주석 추가

--- bucket_list/src/main/java/com/team9/bucket_list/service/AlarmService.java
+++ bucket_list/src/main/java/com/team9/bucket_list/service/AlarmService.java
@@ -33,6 +33,9 @@ public class AlarmService {
         return AlarmListResponse.toList(alarmRepository.findAllByMember_IdAndReadStatusContains(memberId,pageable, (byte)0));
     }
 
+    //sender : 댓글이나 좋아요, 신청서를 작성한 사람, 알람을 보내는 사람의 아이디
+    //postId : 해당 포스트
+    //카테고리 :  0:댓글, 1:좋아요, 2:참가자가 신청서 작성, 3:신청서 승낙, 4:기타
     @Async
     @Transactional
     public void sendAlarm(Long senderId, Long postId, byte category) {
