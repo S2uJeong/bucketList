740103f282ed8a74808bd58c5b549f888b88dfc3
Bae Ji Won <qowhxk@naver.com>
2023-01-30 14:19:59 +0900

    🚀[feat] PostRepository

--- bucket_list/src/main/java/com/team9/bucket_list/repository/PostRepository.java
+++ bucket_list/src/main/java/com/team9/bucket_list/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.team9.bucket_list.repository;
 import com.team9.bucket_list.domain.entity.Post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
 
 }
