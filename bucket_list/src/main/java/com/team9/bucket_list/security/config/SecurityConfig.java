7825eb9a13e851b0ac162394ea5daf2b66514ab1
박은빈 <joypeb@naver.com>
2023-02-01 15:40:11 +0900

    🚀[feat] 모든 경로 혀용

--- bucket_list/src/main/java/com/team9/bucket_list/security/config/SecurityConfig.java
+++ bucket_list/src/main/java/com/team9/bucket_list/security/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,12 +44,9 @@ public class SecurityConfig {
 
                 .exceptionHandling()
                 .authenticationEntryPoint(new CustomAuthenticationEntryPoint()).and()
-
                 .authorizeHttpRequests(authorize -> authorize
                         .shouldFilterAllDispatcherTypes(false)
                         .requestMatchers("/**")
-                        .permitAll()
-                        .requestMatchers("/","/post/**","/comment/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
