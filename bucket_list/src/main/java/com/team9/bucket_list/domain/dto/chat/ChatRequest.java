a0a6ed6255cf1306f53bc3bc2da77c5a9db5f2f4
박은빈 <joypeb@naver.com>
2023-02-02 13:41:48 +0900

    🚀[feat] chatRequest memberId로 변경

--- bucket_list/src/main/java/com/team9/bucket_list/domain/dto/chat/ChatRequest.java
+++ bucket_list/src/main/java/com/team9/bucket_list/domain/dto/chat/ChatRequest.java
@@ -2,11 +2,13 @@ package com.team9.bucket_list.domain.dto.chat;
 
 import com.team9.bucket_list.domain.enumerate.ChatType;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class ChatRequest {
     private Long roomId;
-    private Long userId;
+    private Long memberId;
     private String userName;
     private ChatType chatType;
     private String message;
