55e14055d008d094ec1d49672cf28116b661ee2c
KoKwanwun <dkfajflsla@naver.com>
2023-02-05 18:07:44 +0900

    🚀[fix] SNS 로그인, 일반회원 계정 중복 오류 수정 - 구글, 네이버 이메일로 회원가입한 계정이 존재할 경우, SNS 로그인할 때 발생하는 오류 수정

--- bucket_list/src/main/java/com/team9/bucket_list/security/config/SecurityConfig.java
+++ bucket_list/src/main/java/com/team9/bucket_list/security/config/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.team9.bucket_list.security.config;
 
-import com.team9.bucket_list.security.OAuth2AuthenticationSuccessHandler;
+import com.team9.bucket_list.security.oauth.OAuth2AuthenticationFailureHandler;
+import com.team9.bucket_list.security.oauth.OAuth2AuthenticationSuccessHandler;
 
-import com.team9.bucket_list.repository.RefreshTokenRepository;
 import com.team9.bucket_list.security.entrypoint.CustomAuthenticationEntryPoint;
 import com.team9.bucket_list.security.filter.JwtFilter;
 import com.team9.bucket_list.security.utils.JwtUtil;
-import com.team9.bucket_list.service.MemberService;
 import com.team9.bucket_list.service.OAuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,6 +24,7 @@ public class SecurityConfig {
     private final JwtUtil jwtUtil;
     private final OAuthService oAuthService;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
 
     @Bean
@@ -65,7 +63,7 @@ public class SecurityConfig {
                 .userInfoEndpoint().userService(oAuthService)    //provider로부터 획득한 유저정보를 다룰 service단을 지정한다.
                 .and()
                 .successHandler(oAuth2AuthenticationSuccessHandler)      //OAuth2 로그인 성공 시 호출한 handler
-//                .failureHandler(authenticationFailureHandelr)
+                .failureHandler(oAuth2AuthenticationFailureHandler)
                 .and()
                 .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻 입니다.
                 .build();
