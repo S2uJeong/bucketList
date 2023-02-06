43a1c015745afd590b24e0650a1918f265ad9caf
Bae Ji Won <qowhxk@naver.com>
2023-02-06 17:21:08 +0900

    🚀[feat] 포스트 업데이트 요청 DTO 게시물 상태 추가

--- bucket_list/src/main/java/com/team9/bucket_list/domain/dto/post/PostUpdateRequest.java
+++ bucket_list/src/main/java/com/team9/bucket_list/domain/dto/post/PostUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.team9.bucket_list.domain.dto.post;
 
+import com.team9.bucket_list.domain.entity.Member;
 import com.team9.bucket_list.domain.entity.Post;
 import com.team9.bucket_list.domain.enumerate.PostCategory;
 import com.team9.bucket_list.domain.enumerate.PostStatus;
@@ -12,16 +13,15 @@ import lombok.*;
 @Builder
 public class PostUpdateRequest {
 
+    private String status; // 모집중 or 모집완료
     private String title; //제목
-    private String content; //내용
-    private int cost; //비용
-    private String location; //장소
     private String untilRecruit; //모집종료날짜
-    private int entrantNum; //모집인원제한
     private String eventStart; //버킷 시작일
     private String eventEnd; //버킷 종료일
-    private String status; // 모집중 or 모집완료
     private String category; //카테고리
-
+    private int entrantNum; //모집인원제한
+    private int cost; //비용
+    private String location; //장소
+    private String content; //내용
 
 }
