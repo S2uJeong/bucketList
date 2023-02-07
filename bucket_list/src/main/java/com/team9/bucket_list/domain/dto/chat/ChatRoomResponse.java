2598499659181307ffbf9a12052cc1096c49f96f
박은빈 <joypeb@naver.com>
2023-02-07 14:12:08 +0900

    🚀[feat] dto 변경

--- bucket_list/src/main/java/com/team9/bucket_list/domain/dto/chat/ChatRoomResponse.java
+++ bucket_list/src/main/java/com/team9/bucket_list/domain/dto/chat/ChatRoomResponse.java
@@ -5,18 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
+
 @Builder
 @Data
 public class ChatRoomResponse {
     private Long roomId;
     private String roomName;
     private int totalNum;
+    private LocalDateTime lastMessageTime;
+    private String lastMessage;
+    private String lastUserName;
 
     public static Page<ChatRoomResponse> pageList(Page<ChatRoom> chatRooms) {
         return chatRooms.map(chatRoom -> ChatRoomResponse.builder()
                 .roomId(chatRoom.getId())
                 .roomName(chatRoom.getRoomName())
                 .totalNum(chatRoom.getTotalNum())
+                .lastMessageTime(chatRoom.getLastMessageTime())
+                .lastMessage(chatRoom.getLastMessage())
+                .lastUserName(chatRoom.getLastUserName())
                 .build()
         );
     }
