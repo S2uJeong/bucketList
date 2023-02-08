ba50152399f92ceccdc25ac0437827816dc49b02
박은빈 <joypeb@naver.com>
2023-02-08 22:40:16 +0900

    🚀[feat] alarm 새로운 리스트, 무한스크롤, auth 추가

--- bucket_list/src/main/java/com/team9/bucket_list/controller/rest/AlarmController.java
+++ bucket_list/src/main/java/com/team9/bucket_list/controller/rest/AlarmController.java
@@ -9,7 +9,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,30 +27,43 @@ public class AlarmController {
     //알람 리스트 받아오기
     @GetMapping
     @Operation(summary = "알람 리스트 조회", description = "로그인 되어있는 멤버의 id를 이용하여 조회합니다. 최신 알람일수록 위로 정렬됩니다.")
-    public Response alarmList(@Parameter(hidden = true) @PageableDefault(size = 10) /* @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC)*/ Pageable pageable) {
-        //로그인된 아이디의 알람 리스트
-        Long memberId = 1L;
-
-        return Response.success(alarmService.alarmList(pageable, memberId));
+    public Response alarmList(@Parameter(hidden = true) @PageableDefault(size = 10) @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable, Authentication authentication) {
+        log.info("알람 리스트 컨트롤러");
+        return Response.success(alarmService.alarmList(pageable, Long.valueOf(authentication.getName())));
     }
 
     //알람 카운트 받아오기
     @GetMapping("count")
     @Operation(summary = "알람 카운트 조회", description = "로그인 되어있는 멤버의 id를 이용하여 조회합니다.")
-    public Response alarmCount() {
-        //로그인된 아이디의 알람 리스트
-        Long memberId = 1L;
-
-        return Response.success(alarmService.alarmCount(memberId));
+    public Response alarmCount(Authentication authentication) {
+        return Response.success(alarmService.alarmCount(Long.valueOf(authentication.getName())));
     }
 
-    //알람을 읽었을대
-    @GetMapping("/{alarmId}")
-    @Operation(summary = "특정 알람 조회", description = "알람 id를 이용하여 조회합니다.")
-    public Response alarmRead(@Parameter(name = "alarmId", description = "알람 id", in = ParameterIn.PATH) @PathVariable Long alarmId) {
-        //로그인된 아이디의 알람 리스트
-        Long memberId = 1L;
+    //알람을 읽었을때
+    @PostMapping("/read")
+    @Operation(summary = "알람 읽음 처리", description = "알람 id를 이용하여 조회합니다.")
+    public Response alarmRead(@RequestParam Long alarmId, Authentication authentication) {
+        return Response.success(alarmService.alarmRead(Long.valueOf(authentication.getName()), alarmId));
+    }
 
-        return Response.success(alarmService.alarmRead(memberId, alarmId));
+    //새로운 알림이 등록되었을때
+    @GetMapping("/new")
+    @Operation(summary = "새로운 알림 불러오기", description = "알람 id와 memberId를 이용하여 조회합니다.")
+    public Response newAlarmList(@Parameter(hidden = true) @PageableDefault(size = 10) @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable, Authentication authentication, @RequestParam Long id) {
+        return Response.success(alarmService.newAlarmList(pageable, Long.valueOf(authentication.getName()),id));
+    }
+
+    //스크롤로 새로운 알람을 받아올때
+    @GetMapping("/new/scroll")
+    @Operation(summary = "새로운 알림 불러오기", description = "알람 id와 memberId를 이용하여 조회합니다.")
+    public Response newAlarmListScroll(@Parameter(hidden = true) @PageableDefault(size = 10) @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable, Authentication authentication, @RequestParam Long id) {
+        return Response.success(alarmService.newAlarmListScroll(pageable, Long.valueOf(authentication.getName()),id));
+    }
+
+    //알람 전부 읽기
+    @PostMapping("/read-all")
+    @Operation(summary = "알람 모두 읽기", description = "memberId를 이용하여 수정합니다.")
+    public Response realAllAlarm(Authentication authentication) {
+        return Response.success(alarmService.realAllAlarm(Long.valueOf(authentication.getName())));
     }
 }
