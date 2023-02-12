85d571292c9aeb78f9cd5333ae34442c97e77ffd
KoKwanwun <dkfajflsla@naver.com>
2023-02-12 10:13:15 +0900

    🐞[fix] 의존성 에러 수정 - ProfileRepository 어노테이션 추가

--- bucket_list/src/main/java/com/team9/bucket_list/repository/ProfileRepository.java
+++ bucket_list/src/main/java/com/team9/bucket_list/repository/ProfileRepository.java
@@ -2,9 +2,11 @@ package com.team9.bucket_list.repository;
 
 import com.team9.bucket_list.domain.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long>{
 
     Optional<Profile> findByMember_Id(Long memberId);
