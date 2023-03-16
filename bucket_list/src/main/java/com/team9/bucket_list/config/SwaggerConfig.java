1571361b3239f04c1a6b0fb67145314dfe938d32
sujeong <sj980204@gmail.com>
2023-03-17 02:08:06 +0900

    🐞[fix] swagger 입력 시, fetch 에러 뜨는 것 고침

--- bucket_list/src/main/java/com/team9/bucket_list/config/SwaggerConfig.java
+++ bucket_list/src/main/java/com/team9/bucket_list/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,7 +20,7 @@ public class SwaggerConfig {
                 .description("✨ Let's find BUCKETLIST with us ✨");
 
         return new OpenAPI()
-
+                .addServersItem(new Server().url("/"))
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .components(new Components()
